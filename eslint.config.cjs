6dd5eeb2b2b6b64b64a021c59579f023815d2963
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2026-06-11 22:12:01 +0200

    feat: add nx to project

---
+++ eslint.config.cjs
@@ -0,0 +1,41 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  js.configs.recommended,
+
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+
+    rules: {
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
+
+      semi: ['error', 'always'],
+
+      eqeqeq: ['error', 'always'],
+
+      curly: ['error', 'all'],
+
+      '@typescript-eslint/explicit-function-return-type': 'error',
+
+      '@typescript-eslint/consistent-type-imports': 'error',
+
+      '@typescript-eslint/no-floating-promises': 'error',
+
+      '@typescript-eslint/no-misused-promises': 'error',
+    },
+  }
+);
