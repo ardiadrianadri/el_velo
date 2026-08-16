44df158f79e63bee1afba69328d2bd76fe86a103
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2026-08-16 06:58:55 +0200

    Revert "fix: get AI analysis parameters"
    This reverts commit fd43bb5cce55b1930962806410fad16488783a1f.

--- packages/tools/typescript-review/src/helpers/getAIAnalysis.ts
+++ packages/tools/typescript-review/src/helpers/getAIAnalysis.ts
@@ -3,8 +3,6 @@ import OpenAI from 'openai';
 import { config, reviewSchema } from '../config.js';
 
 export async function getAIAnalysis(instructions: string, diffInput: string): Promise<string> {
-    console.log('NNNN instructionss: ', instructions);
-    console.log('NNNN diffInput: ', diffInput);
     const openai = new OpenAI({ apiKey: config.openAiApiKey });
     const response = await openai.responses.create({
         model: config.openAIModel,
